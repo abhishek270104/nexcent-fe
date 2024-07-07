673cfa5974ce147f9fea1f4c26c1db1b9845e100
abhishek270104 <abhishek.2024cs1184@kiet.edu>
2024-07-07 20:52:34 +0530

    Theming issues fixed

--- app/layout/header.tsx
+++ app/layout/header.tsx
@@ -1,38 +1,44 @@
 'use client'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/navbar';
 import { useTheme } from 'next-themes';
 import { UseThemeProps } from 'next-themes/dist/types';
 
 const Header = () => {
   const { resolvedTheme, theme, setTheme }: UseThemeProps = useTheme()
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true)
+    setTheme("light")
+  }, []);
   const toggleTheme = (prev: string | undefined) => {
-    if (prev === undefined) {
-      setTheme("light")
-    }
     if (prev === "light") {
       setTheme("dark")
+      document.body.classList.add("dark")
     }
     else {
       setTheme("light")
+      document.body.classList.remove("dark")
     }
   }
   return (
-    <header className='header'>
-      <div className='logo'>
-        <img src='/images/logo.svg' alt="logo" />
-        <span className='logoText'>Nexcent</span>
-      </div>
-      <Navbar />
-      <div className='signinButtons'>
-        <button className='loginbutton'>Login</button>
-        <button className='signupbutton'>Signup</button>
-        <span className='theme'>
-          <img src={resolvedTheme === "light" ? "/images/mode-light.svg" : "/images/dark-theme.svg"} onClick={() => toggleTheme(resolvedTheme)} />
-        </span>
-      </div>
+    <>
+      {mounted ? <header className='header'>
+        <div className='logo'>
+          <img src={resolvedTheme == "light" ? '/images/logo.svg' : '/images/logo-white.svg'} alt="logo" />
+          <span className='logoText'>Nexcent</span>
+        </div>
+        <Navbar />
+        <div className='signinButtons'>
+          <button className='loginbutton'>Login</button>
+          <button className='signupbutton'>Signup</button>
+          <span className='theme'>
+            <img src={resolvedTheme == "light" ? "/images/mode-light.svg" : "/images/dark-theme.svg"} onClick={() => toggleTheme(resolvedTheme)} />
+          </span>
+        </div>
 
-    </header>
+      </header> : null}
+    </>
   )
 }
 
