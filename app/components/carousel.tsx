d66d872ed04daec78578ee84da728aa6a42fbecf
abhishek270104 <abhishek.2024cs1184@kiet.edu>
2024-07-07 17:32:01 +0530

    Carousel implemented using library

--- app/components/carousel.tsx
+++ app/components/carousel.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
-
+'use client'
+import { carouselSlides } from '@/public/helper';
+import React, { useState } from 'react'
+import ReactSimplyCarousel from 'react-simply-carousel';
 const Carousel = () => {
+  const [active, setActive] = useState<number>(0);
   return (
-    <div>Carousel</div>
+    <>
+      <section className="carouselSlide" >
+        <ReactSimplyCarousel
+          activeSlideIndex={active}
+          onRequestChange={setActive}
+          itemsToShow={1}
+          itemsToScroll={1}
+          speed={600}
+          easing="linear"
+        >
+          {carouselSlides.map((carouselSlide) => (
+            <React.Fragment key={carouselSlide.id}>
+              {carouselSlide.component}
+            </React.Fragment>
+          ))}
+        </ReactSimplyCarousel>
+      </section>
+      <div className=" dots mx-auto mb-10">
+        {carouselSlides.map((_, index: number) => (
+          <button className={`dot ${active === index ? "active" : ""}`} key={index} onClick={() => setActive(index)}></button>
+        ))}
+      </div>
+    </>
+
   )
 }
 
--- public/helper.tsx
+++ public/helper.tsx
@@ -1,9 +1,15 @@
+
 export const format = (number: number) => {
     return String(number).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 export interface IClient {
     name: string,
     imageURL: string,
+}
+interface ICarousel {
+    id: number,
+    component: React.ReactElement,
+    url: string,
 }
 export const clientData: IClient[] = [
     { name: "client1", imageURL: "/images/client-icon1.svg" },
@@ -14,3 +20,50 @@ export const clientData: IClient[] = [
     { name: "client6", imageURL: "/images/client-icon6.svg" },
     { name: "client7", imageURL: "/images/client-icon3.svg" },
 ];
+export const carouselSlides: ICarousel[] = [
+    {
+        id: 1,
+        component: <div className='slide mx-auto mb-10'>
+            <div className=''>
+                <h2 className="heading">Lessons and insights 1</h2>
+                <h2 className="duration">from 8 years</h2>
+                <p className="my-5">Where to grow your business as a photographer: site or social media?</p>
+                <button className="greenButton">Register</button>
+            </div>
+            <div>
+                <img src="/images/carousel-template-image.svg" alt="carousel_image" />
+            </div>
+        </div> ,
+        url: "",
+    },
+    {
+        id: 2,
+        component: <div className='slide mx-auto mb-10'>
+            <div>
+                <img src="/images/carousel-template-image.svg" alt="carousel_image" />
+            </div>
+            <div className=''>
+                <h2 className="heading">Lessons and insights 2</h2>
+                <h2 className="duration">from 16 years</h2>
+                <p className="my-5">Where to grow your business as a photographer: site or social media?</p>
+                <button className="greenButton">Register</button>
+            </div>
+        </div>,
+        url: "",
+    },
+    {
+        id: 3,
+        component: <div className='slide mx-auto mb-10'>
+            <div className=''>
+                <h2 className="heading">Lessons and insights 3</h2>
+                <h2 className="duration">from 4 years</h2>
+                <p className="my-5">Where to grow your business as a photographer: site or social media?</p>
+                <button className="greenButton">Register</button>
+            </div>
+            <div>
+                <img src="/images/carousel-template-image.svg" alt="carousel_image" />
+            </div>
+        </div>,
+        url: "",
+    },
+]
